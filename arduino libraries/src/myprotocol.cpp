e0992d9f1d1a30cf2b893dcc4acf487b8db63add
Isaranu Janthong <isaranujanthong@yahoo.com>
2017-11-10 14:35:07 +0700

    Update myprotocol.cpp

--- arduino libraries/src/myprotocol.cpp
+++ arduino libraries/src/myprotocol.cpp
@@ -7,7 +7,7 @@ created : 2017.Nov.11
 
 #include "myprotocol.h"
 
-#define host "xx.xx.xx.xx" /* เลข IP address server ของเรา */
+#define host "35.198.221.246"
 #define port 4000
 #define libversion "v1.0"
 
@@ -70,19 +70,23 @@ String myprotocol::sayhi(){
 
 String myprotocol::WriteDashboard(float val){
 
-    _val = val;
+    _val = "val=" + val;
 
     WiFiClient client;
     if(client.connect(host, port)){
-      _str = "GET /write/";
-      _str += _val;
+      _str = "POST /write/";
       _str += " HTTP/1.1\r\n";
       _str += "Host: ";
       _str += host;
       _str += ":";
       _str += port;
       _str += "\r\n";
-      _str += "Connection: keep-alive\r\n\r\n";
+      _str += "Cache-Control: no-cache\r\n";
+      _str += "Content-Type: application/x-www-form-urlencoded";
+      _str += "Content-Length: ";
+      _str += _val.length();
+      _str += "\r\n";
+      _str += _val;
 
       client.print(_str);
 
@@ -101,3 +105,17 @@ String myprotocol::WriteDashboard(float val){
 String myprotocol::getVersion(){
   return pn_libversion;
 }
+
+/*
+
+https://github.com/esp8266/Arduino/issues/1390
+
+client.println("POST /posts HTTP/1.1");
+client.println("Host: jsonplaceholder.typicode.com");
+client.println("Cache-Control: no-cache");
+client.println("Content-Type: application/x-www-form-urlencoded");
+client.print("Content-Length: ");
+client.println(PostData.length());
+client.println();
+client.println(PostData);
+*/
