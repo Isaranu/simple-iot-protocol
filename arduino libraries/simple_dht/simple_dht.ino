1cdc1a5efadb5ad373e729d0ca7115964cada2da
Isaranu Janthong <isaranujanthong@yahoo.com>
2017-11-22 14:11:34 +0700

    Create simple_dht.ino

---
+++ arduino libraries/simple_dht/simple_dht.ino
@@ -0,0 +1,30 @@
+#include "DHT.h"
+
+#define DHTPIN 1
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+float t,h;
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}  
+
+void loop() {
+  
+  /* - DHT sensor reading - */
+  t = dht.readTemperature();
+  h = dht.readHumidity();
+
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.println("Temperature is " + String(t) + " celcuis");
+  Serial.println("Humidity is " + String(h) + " %RH");
+  Serial.println("----------------------------------------");
+ 
+  delay(3000);
+}
