5b33849bdad803c98144aba458b086c553a78670
Isaranu Janthong <isaranujanthong@yahoo.com>
2017-12-12 01:08:22 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,7 +12,9 @@ var port = 4000;
 var mongojs = require('mongojs');
 var Promise = require('promise');
 var myiotdb = mongojs('myiotdb');
+var dhtdb = mongojs('dht');
 var devid, data, datasize, dataset='';
+var t, h;
 
 app.get('/', function (req, res) {
   res.send("my iot Protocol ready !");
@@ -30,6 +32,23 @@ app.get('/read/:datasize', function (req, res) {
   var strReadReq = JSON.parse(strParseReadReq);
   datasize = strReadReq.datasize;
   readdata(datasize, res);
+});
+
+/* For DHT write */
+app.get('/writedht/:t/:h', function (req, res) {
+  var strParseWriteReq = JSON.stringify(req.params);
+  var strWriteReq = JSON.parse(strParseWriteReq);
+  t = strWriteReq.t;
+  h = strWriteReq.h;
+  writeDHT(t, h, res);
+});
+
+/* For DHT data read */
+app.get('/readdht/:datasize', function (req, res) {
+  var strParseReadReq = JSON.stringify(req.params);
+  var strReadReq = JSON.parse(strParseReadReq);
+  datasize = strReadReq.datasize;
+  readDHT(datasize, res);
 });
 
 app.listen(port, function () {
@@ -69,6 +88,43 @@ function readDataFromMongo(_readdatasize, res){
   return new Promise(function(resolve,reject){
     var myreadcollection = myiotdb.collection('mycollection');
     myreadcollection.find({}).limit(Number(_readdatasize)).sort({recordTime: 1}, function(err, docs){
+      //console.log(JSON.stringify(docs));
+      res.jsonp(docs);
+    });
+  });
+}
+
+async function writeDHT(_t, _h, res){
+  await writeDHTtoMongo(_t, _h, res);
+}
+
+function writeDHTtoMongo(_saveT, _saveH, res){
+  return new Promise(function(resolve, reject){
+    var dhtwritecollection = dhtdb.collection('dhtcol');
+    dhtwritecollection.insert({
+      t: Number(_saveT),
+      h: Number(_saveH),
+      recordTime: new Date().getTime()
+    }, function(err){
+      if(err){
+        console.log(err);
+        res.send(String(err));
+      }else {
+        console.log('record dht data ok');
+        res.send('record dht data ok');
+      }
+    });
+  });
+}
+
+async function readDHT(_datasize, res){
+  await readDHTFromMongo(_datasize, res);
+}
+
+function readDHTFromMongo(_readdatasize, res){
+  return new Promise(function(resolve,reject){
+    var dhtcollection = dhtdb.collection('dhtcol');
+    dhtcollection.find({}).limit(Number(_readdatasize)).sort({recordTime: -1}, function(err, docs){
       //console.log(JSON.stringify(docs));
       res.jsonp(docs);
     });
